875434ab8c995a2653653812f77c703851d553b9
FurryBoyYT <bogdanfurry2000@gmail.com>
2024-12-12 22:07:38 +0200

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,14 +1,17 @@
 -- made by furryboy, fuck you skids
 return {
-    ["credit"] = {
-        ["name"] = "Furryboy",
+    total_cash = 2190000,
+    credit = {
+        name = "Furryboy",
     },
-    "TRADEME!",
-    "DAUP",
-    "pumpkins2023",
-    "RUBY",
-    "DACARNIVAL",
-    "HALLOWEEN2024",
-    "THANKSGIVING24",
-    "EMERALD"
+    codes = {
+        "TRADEME!",
+        "DAUP",
+        "pumpkins2023",
+        "RUBY",
+        "DACARNIVAL",
+        "HALLOWEEN2024",
+        "THANKSGIVING24",
+        "EMERALD"
+    }
 }
