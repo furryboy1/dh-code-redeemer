e3c2979c8b8e3295c33c3beafe8f6ca49c8efbf5
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2024-12-16 14:34:07 +0200

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,7 +1,7 @@
 -- made by furryboy, fuck you skids
 -- https://github.com/FurryBoyYT/dh-code-redeemer/raw/refs/heads/main/codes.lua
 return {
-    totalcash = 4078000,
+    totalcash = 3878000,
     credit = "FurryBoy",
     codes = {
         "TRADEME!",
