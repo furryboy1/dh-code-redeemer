468d220c5c1eff4c34856a99efc9f0a1aa6c01e6
FurryBoyYT <bogdanfurry2000@gmail.com>
2025-03-08 17:48:29 +0200

    Updated list with check (forgot to update it yesterday)

--- codes.lua
+++ codes.lua
@@ -1,22 +1,17 @@
 -- made by furryboy, fuck you skids
 -- https://github.com/FurryBoyYT/dh-code-redeemer/raw/refs/heads/main/codes.lua
 return {
-    totalcash = 3690000,
-    credit = "FurryBoy",
+    totalcash = 2440000,
+    credit = 'FurryBoy',
     codes = {
-        "TRADEME!",
-        "DAUP",
-        "pumpkins2023",
-        "RUBY",
-        "DACARNIVAL",
-        "HALLOWEEN2024",
-        "THANKSGIVING24",
-        "SHRIMP",
-        "VIP",
-        "2025",
-        "Beary",
-        "ShortCake",
-        "VALENTINES2025",
-        "BLOSSOM"
+        'TRADEME!',
+        'pumpkins2023',
+        'DACARNIVAL',
+        'HALLOWEEN2024',
+        'SHRIMP',
+        'VIP',
+        '2025',
+        'Duck',
+        'Watch'
     }
 }
