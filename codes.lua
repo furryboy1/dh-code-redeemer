2d3101084800ffe7eda764d8a53c1484e0e6e3d1
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-05-24 04:23:45 +0300

    o

--- codes.lua
+++ codes.lua
@@ -1,4 +1,4 @@
--- made by furryboy, fuck you skids
+-- Made by FurryBoy
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
     totalcash = 3990000,
