abf48299ba87042b51cafe0c4a40ed81a7a58141
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-06-05 16:27:19 +0300

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,24 +1,10 @@
 -- Made by FurryBoy
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
-    totalcash = 3990000,
+    totalcash = 500000,
     credit = 'FurryBoy',
     rate = 10,
     codes = {
-        'TRADEME!',
-        'DAUP',
-        'pumpkins2023',
-        'DACARNIVAL',
-        'THANKSGIVING24',
-        'HALLOWEEN2024',
-        'RUBY',
-        'SHRIMP',
-        'VIP',
-        '2025',
-        'Duck',
-        'Watch',
-        'Quack',
-        'Easter25',
         'Arcane',
         'Samurai'
     }
