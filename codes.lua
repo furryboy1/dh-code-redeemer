65053c67ba341f47d2989d60c82f753ca3b7c8aa
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-08-17 11:39:25 +0300

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -2,11 +2,11 @@
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
     totalcash = 1150000,
-    credit = 'FurryBoy',
+    credit = "FurryBoy",
     rate = 5,
     codes = {
-        'BUBU',
-        'MUSIC',
-        'KINGPIN'
+        "BUBU",
+        "MUSIC",
+        "KINGPIN"
     }
 }
