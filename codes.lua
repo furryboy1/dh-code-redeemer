715777552bf9f621afb9f2a75510c0c5a571edd0
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-08-17 10:23:37 +0300

    Updated

--- codes.lua
+++ codes.lua
@@ -1,14 +1,10 @@
 -- Made by FurryBoy
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
-    totalcash = 1550000,
+    totalcash = 1150000,
     credit = 'FurryBoy',
     rate = 5,
     codes = {
-        'Arcane',
-        'Samurai',
-        'Summer25',
-        'Jellyfish',
         'BUBU',
         'MUSIC',
         'KINGPIN'
