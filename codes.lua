d676c857d114aef4201e69f36ee0d18d5aa068e9
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2025-10-03 11:23:29 +0300

    Update codes.lua

--- codes.lua
+++ codes.lua
@@ -1,13 +1,14 @@
 -- Made by FurryBoy
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
-    totalcash = 948999,
+    totalcash = 1948999,
     credit = "FurryBoy",
     rate = 5,
     codes = {
         "BRAINROT",
         "LUXE",
         "TURBO",
-        "Sep2025"
+        "Sep2025",
+        "OCTOBER25"
     }
 }
