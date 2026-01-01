49cfb2ee37b36a155898d26860f7b1d94c1008c7
FurryBoy <104305700+FurryBoyYT@users.noreply.github.com>
2026-01-01 13:22:24 +0200

    i forgot to add a code last month in 2025 on github

--- codes.lua
+++ codes.lua
@@ -1,12 +1,14 @@
 -- Made by FurryBoy
 -- https://raw.githubusercontent.com/furryboy1/dh-code-redeemer/refs/heads/main/codes.lua
 return {
-    totalcash = 900000,
+    totalcash = 1352600,
     credit = "FurryBoy",
     rate = 5,
     codes = {
         "Sep2025",
         "XMAS2025",
-        "HAPPYHOLIDAYS25"
+        "HAPPYHOLIDAYS25",
+        "XMASEVE25",
+        "HAPPYNEWYEAR26"
     }
 }
